a0f0792bf022542f838c07e572c831084d9b708e
Jake Schaffer <imjakedev@gmail.com>
2023-10-06 14:28:21 -0500

    Updates styles to use scss and not css modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image';
-import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.center}>
+    <main className="main">
+      <div className="center">
         <Image
-          className={styles.logo}
+          className="logo"
           src="/sublimemeh-logo.svg"
           alt="SublimeMeh Logo"
           width={393.79}
